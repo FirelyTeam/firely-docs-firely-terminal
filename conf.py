8496838820fb2cd6c4262cc6f618bd4cf3809857
KhristinaLukanets <khlukanets@gmail.com>
2026-08-10 09:50:59 +0300

    Add new banner component and update styles, change favicon

--- conf.py
+++ conf.py
@@ -37,7 +37,7 @@ intersphinx_mapping = {
     'simplifier_docs': ('https://docs.fire.ly/projects/Simplifier/', None)
 }
 
-    
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -59,6 +59,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_favicon = '_static/images/favicon.svg'
 
 master_doc = 'index'
 html_theme_options = {
